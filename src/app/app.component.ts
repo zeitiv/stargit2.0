a11ff7f1986ede090312d28cda5aeb40b5a0850e
theargex <eragonder@gmail.com>
2019-10-04 11:54:10 +0200

    create basic layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NbSidebarService, NbThemeService } from '@nebular/theme';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(private sidebarService: NbSidebarService, private themeService: NbThemeService) {}
   title = 'stargit';
+  sun = 'sun-outline';
+  moon = 'moon-outline';
+  icon = this.moon;
+
+  items = [
+    {
+      title: 'Trending',
+      icon: 'star-outline',
+      link: [],
+    },
+    {
+      title: 'Discord',
+      icon: 'message-square-outline',
+      link: [],
+    },
+    {
+      title: 'About',
+      icon: 'info-outline',
+      link: [],
+    },
+    {
+      title: 'Github',
+      icon: 'github-outline',
+      url: 'https://github.com/theArgex/stargit2.0',
+      target: '_blank'
+    },
+  ];
+
+  toggle() {
+    this.sidebarService.toggle(false, 'left');
+  }
+
+  changeTheme() {
+    if (this.icon === this.moon) {
+      this.themeService.changeTheme('default');
+      this.icon = this.sun;
+    } else {
+      this.themeService.changeTheme('dark');
+      this.icon = this.moon;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { NbThemeModule, NbLayoutModule, NbSearchModule, NbButtonModule, NbSidebarModule, NbIconModule, NbMenuModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    NbThemeModule.forRoot({ name: 'cosmic' }),
+    NbThemeModule.forRoot({ name: 'dark' }),
     NbLayoutModule,
-    NbEvaIconsModule
+    NbEvaIconsModule,
+    NbSearchModule,
+    NbButtonModule,
+    NbSidebarModule.forRoot(),
+    NbSidebarModule,
+    NbMenuModule.forRoot(),
+    NbEvaIconsModule,
+    NbIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]

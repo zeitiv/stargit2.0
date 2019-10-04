162821684ece55e24c1b8c092f95d571ea3083c6
theargex <eragonder@gmail.com>
2019-10-04 17:33:56 +0200

    fix routing locally

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '', component: HomeComponent, pathMatch: 'full'},
   {path: 'home', component: HomeComponent}
 ];
 
